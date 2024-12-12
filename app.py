75a3eea9f4a98466c324662bb70aa0a0d1502fa5
247630 Bień Adrian <247630@edu.p.lodz.pl>
2024-12-12 23:36:35 +0100

    jest polaczenie z baza i dodany rekord

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker, declarative_base, Mapped
+from sqlalchemy.testing.schema import mapped_column
 
 app = Flask(__name__)
 
+db = sa.create_engine('sqlite:///db.sqlite')
+Session = sessionmaker(bind=db)
+Base = declarative_base()
+
+
+class Point(Base):
+    __tablename__ = 'points'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    x: Mapped[float]
+    y: Mapped[float]
+    cat: Mapped[int]
+
+    def __repr__(self):
+        return f'<Point ({self.x}, {self.y}), cat: {self.cat}>'
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -9,4 +26,11 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    Base.metadata.create_all(bind=db)
+    point1 = Point(x=1, y=2, cat=3)
+
+    with Session() as session:
+        session.add(point1)
+        session.commit()
+        print(session.query(Point).all())
