4f1e4ed973793afc2bc32974f6857ed0d7a1406d
adrianbien <247630@edu.p.lodz.pl>
2024-12-13 13:17:20 +0100

    podstawowe wyswietlenie danych z bazy

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker, declarative_base, Mapped
@@ -20,17 +22,38 @@ class Point(Base):
     def __repr__(self):
         return f'<Point ({self.x}, {self.y}), cat: {self.cat}>'
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+    def to_JSON(self):
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__,
+            sort_keys=True,
+            indent=4)
+
+@app.route('/', methods=['GET'])
+def display_points():
+    try:
+        with Session() as session:
+            points = session.query(Point).all()
+
+            point_text = '<ul>'
+            for point in points:
+                point_text += '<li>' + str(point.x) + ', ' + str(point.y) + ', ' + str(point.cat) +'</li>'
+            point_text += '</ul>'
+            return point_text
+
+    except Exception as e:
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 
 if __name__ == '__main__':
-    # app.run()
-    Base.metadata.create_all(bind=db)
-    point1 = Point(x=1, y=2, cat=3)
+    app.run()
+    # Base.metadata.create_all(bind=db)
+    # point1 = Point(x=5, y=2, cat=1)
 
-    with Session() as session:
-        session.add(point1)
-        session.commit()
-        print(session.query(Point).all())
+
+    # with Session() as session:
+    #     session.add(point1)
+    #     session.commit()
+    #     print(session.query(Point).all())
